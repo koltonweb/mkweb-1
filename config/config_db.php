7e5d9a60fdc20ab71db10e46d048c5b0a45f8bf0
Maxkolton <47429282+Maxkolton@users.noreply.github.com>
2019-10-24 18:08:52 +0300

    Update config_db.php

--- config/config_db.php
+++ config/config_db.php
@@ -3,7 +3,7 @@
 // вызывается из класса DB в /vendor/mkweb/core/DB.php
 return [
     'dsn'       => 'mysql:host=localhost;dbname=kostin', 
-    'username'  => 'maxkoltan',
-    'password'  => 'l;fdfcrhbgncj,frf0204smeshariki21',
+    'username'  => 'root',
+    'password'  => '',
     'except'    => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 ];
